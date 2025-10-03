95f170d2bbcc41dc634fb921f20503c3fc91bb15
marcus-justice31 <marcusuy@crescentschool.org>
2025-10-03 18:52:19 -0400

    parameters for prompt

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,18 @@ client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
 )
 
+gender = ""
+age = ""
+height_cm = 0
+weight_kg = 0
+body_fat_percentage = 0
+activity_level = ""
+goal = ""
+diatary_restrictions = ""
+meals_per_day = 0
+
+
+
 response = client.responses.create(
     model="gpt-5-nano",
     input="Write me a 3 line poem about the colour blue"
