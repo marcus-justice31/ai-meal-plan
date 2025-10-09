97c81983e5f2e533b29426137c7e778321370bc8
marcus-justice31 <marcusuy@crescentschool.org>
2025-10-09 12:36:31 -0400

    changed Components to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 import Login from './Components/Login';
 
 function App() {
